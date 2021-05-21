99c9e0296614ab66a326ced555948b586a32c488
Luana Nalon <luananalon@hotmail.com>
2021-05-21 16:30:54 +0100

    Prepare Data for show on tableview

--- DreamLister/AppDelegate.swift
+++ DreamLister/AppDelegate.swift
@@ -78,4 +78,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
+let ad = UIApplication.shared.delegate as! AppDelegate
+let context = ad.persistentContainer.viewContext
 
---
+++ DreamLister/View_/ItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Luana Nalon on 20/05/2021.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+}
